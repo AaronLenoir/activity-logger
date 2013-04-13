63d8e9dc3abf78a9952674053fe737c589c82234
Aaron Lenoir <lenoir.aaron@gmail.com>
2013-04-14 00:12:42 +0200

    Added a very basic installer with BSD EULA. Still need to add a flag for starting automatically at windows startup.

--- ActivityLogger.Installer/createInstaller.cmd
+++ ActivityLogger.Installer/createInstaller.cmd
@@ -1,6 +1,5 @@
 @echo off
-candle "ActivityLogger 1.0.0.wxs" "WixUI_ActivityLogger.wxs"
-light "ActivityLogger 1.0.0.wixobj" "WixUI_ActivityLogger.wixobj" -ext WixUIExtension -out "ActivityLogger 1.0.0.msi"
+candle "ActivityLogger 1.0.0.wxs"
+light "ActivityLogger 1.0.0.wixobj" -ext WixUIExtension -dWixUILicenseRtf=License.rtf -out "ActivityLogger 1.0.0.msi"
 del "ActivityLogger 1.0.0.wixobj"
-del "WixUI_ActivityLogger.wixobj"
 del "ActivityLogger 1.0.0.wixpdb"
