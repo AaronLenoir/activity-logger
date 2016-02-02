41f5d75e0568af2a4afdd6da2575ccc696148a3a
lenoir.aaron@gmail.com <lenoir.aaron@gmail.com>
2016-02-02 23:46:23 +0100

    Fixes #9 Updates the installer to upgrade 1.0.0 to 1.1.0

--- ActivityLogger.Installer/createInstaller.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set version=1.0.11
-candle "ActivityLogger %version%.wxs" -ext WixUtilExtension
-light "ActivityLogger %version%.wixobj" -ext WixUIExtension -ext WixUtilExtension -dWixUILicenseRtf=License.rtf -out "ActivityLogger %version%.msi"
-del "ActivityLogger %version%.wixobj"
-del "ActivityLogger %version%.wixpdb"
-pause
